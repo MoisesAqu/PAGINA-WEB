1b0d883b6aa3401d9bb7c59b11792337f68d1eb1
MoisesAqu <135161531+MoisesAqu@users.noreply.github.com>
2024-10-19 02:14:56 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,28 +1,14 @@
-// Función para mostrar la sección de tareas
-function irATareas() {
-    document.getElementById("inicio").style.display = "none"; // Oculta la página de inicio
-    document.getElementById("tareas").style.display = "block"; // Muestra la sección de tareas
-}
+// Script para manejar los botones desplegables
+var coll = document.getElementsByClassName("collapsible");
 
-// Función para volver a la página principal
-function volverAPrincipal() {
-    document.getElementById("tareas").style.display = "none"; // Oculta la sección de tareas
-    document.getElementById("inicio").style.display = "block"; // Muestra la página de inicio
+for (var i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
-
-// Código para los botones colapsables
-document.addEventListener("DOMContentLoaded", function() {
-    var coll = document.getElementsByClassName("collapsible");
-    
-    for (let i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.display === "block") {
-                content.style.display = "none";
-            } else {
-                content.style.display = "block";
-            }
-        });
-    }
-});
